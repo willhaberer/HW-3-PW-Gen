bb5587dca013ec8bd919b09e598ec15ce18da7b8
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-10-13 21:01:04 -0400

    Changed UI to show different colors on button press

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+if (gererateBtn.addEventListener)
+  generateBtn.addEventListener("click", promptUser(), false);
+else if (generateBtn.attachEvent)
+  generateBtn.attachEvent("onclick", promptuser());
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var el = document.getElementById("clickMe");
+if (el.addEventListener) el.addEventListener("click", promptUser(), false);
+else if (el.attachEvent) el.attachEvent("onclick", promptUser());
+
+//Creating prompt tree
+function promptUser() {
+  var specialCharacters = prompt(
+    "Would you like to include special characters?"
+  );
+}
