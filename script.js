e559afdce1fa59171eb0f0cb73011794b292f575
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-10-13 21:35:28 -0400

    Linked button press to start prompt function

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-if (gererateBtn.addEventListener)
-  generateBtn.addEventListener("click", promptUser(), false);
-else if (generateBtn.attachEvent)
-  generateBtn.attachEvent("onclick", promptuser());
+generateBtn.addEventListener("click", promptUser);
 
 // Write password to the #password input
 function writePassword() {
@@ -16,10 +12,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-var el = document.getElementById("clickMe");
-if (el.addEventListener) el.addEventListener("click", promptUser(), false);
-else if (el.attachEvent) el.attachEvent("onclick", promptUser());
 
 //Creating prompt tree
 function promptUser() {
