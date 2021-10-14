332c6c1287844da25eda59cf081791cb9c2ef64f
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-10-14 19:51:32 -0400

    Created arrays for num, letters and special chars

--- script.js
+++ script.js
@@ -2,6 +2,68 @@
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", userDefinePass);
 
+var alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+var specialChars = [
+  "!",
+  " ",
+  '"',
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "]",
+  "^",
+  "_",
+  "~",
+];
+
+var numList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -23,6 +85,8 @@ function userDefinePass() {
     var passLength = prompt("Please enter a numeric value between 8 and 128");
   }
 
-  var specialChars = confirm("Would you like to include special characters?");
-  alert(specialChars);
+  var includeSpecialChars = confirm(
+    "Would you like to include special characters?"
+  );
+  alert(includeSpecialChars);
 }
